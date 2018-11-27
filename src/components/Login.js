68cd794e8f643de181be02c9ee96589b047e7b1a
Tiago Marques <thymarques@gmail.com>
2018-11-27 12:10:29 -0800

    some adjusts

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const Login = () => (
+const Login = (props) => (
     <div className="login">
         <div className="picture"></div>
-        <div className="info">
-            <h1>Blog Admin</h1>
-            <form className="loginForm">
-                <input type="text" placeholder="Insert your email" />
-                <input type="password" placeholder="Insert your password" />
-                <button>Log in</button>
-            </form>
+        <div className="info loginContent">
+            <div className="wrapForm">
+                <h1>Blog Admin</h1>
+                <form className="loginForm"
+                        onSubmit={() => {
+                            props.history.push('/admin');
+                        }}
+                >
+                    <input type="text" placeholder="Insert your email" />
+                    <input type="password" placeholder="Insert your password" />
+                    <button>Log in</button>
+                </form>
 
-            <Link to="#forgotpass">Forgot your password </Link> ? <Link to="#forgotemail">or email</Link> ? <Link to="#signin">Sign in</Link>
+                <Link to="#forgotpass">Forgot your password ?</Link> <Link to="#forgotemail">or email ?</Link><br></br><Link to="#signin">Sign in</Link>
+            </div>
         </div>
     </div>
 );
